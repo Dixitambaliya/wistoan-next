f7520601e03524caa72c7f76da8419fb8abe993b
Ambaliya Dixit <154045627+Dixitambaliya@users.noreply.github.com>
2026-02-07 18:23:53 +0530

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://wistoan.vercel.app',
+    generateRobotsTxt: true,
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+};
